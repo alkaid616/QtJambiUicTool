4705469c08bfac34a94eca0940b0f53f990a5fcd
T <2804151138@qq.com>
2024-08-22 02:16:07 +0800

    添加依赖项，打包配置

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,10 +19,24 @@ idea {
 
 dependencies {
     testImplementation(kotlin("test"))
+    // https://central.sonatype.com/artifact/io.qtjambi/qtjambi-native-windows-x64
+    runtimeOnly("io.qtjambi:qtjambi-native-windows-x64:6.7.2")
+    // https://central.sonatype.com/artifact/io.qtjambi/qtjambi-uic
+    implementation("io.qtjambi:qtjambi-uic:6.7.2")
+    // https://central.sonatype.com/artifact/io.qtjambi/qtjambi-uic-native-windows-x64
+    runtimeOnly("io.qtjambi:qtjambi-uic-native-windows-x64:6.7.2")
+    // https://mvnrepository.com/artifact/io.qtjambi/qtjambi-uitools
+    implementation("io.qtjambi:qtjambi-uitools:6.7.2")
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+tasks.shadowJar {
+    manifest {
+        attributes["Main-Class"] = "MainKt"
+    }
+    archiveFileName.set("Qt-Jambi-Uic-Tool-${version}.jar")
 }
 
 kotlin {
